a38139dcce075eb82ad53b27a6bce2093f6156f3
adambsimpson <atj@adambsimpsons-Mac-mini.local>
2014-06-29 22:59:31 -0400

    Changed default resolution

--- renderer.c
+++ renderer.c
@@ -15,8 +15,8 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 
     FRAC_INFO frac_left, frac_right;
-    init_fractal(&frac_left, true, 300);
-    init_fractal(&frac_right, true, 300);
+    init_fractal(&frac_left, true, 1300);
+    init_fractal(&frac_right, true, 1300);
 
     if (myrank == 0){
 
